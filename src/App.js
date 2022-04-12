25cf73ad6ec6fd3dacbea752590f8d970c856e83
Billie Amit <biliamit@gmail.com>
2022-04-12 14:41:09 +0300

    add function to count the text number of words

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ function App() {
     setText(value);
   }
 
+  function wordsCounter(str) {
+    // return str.split(' ').length //! this solution has some bugs of counting empty spaces
+    const wordsArr = str.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
       <textarea onChange={handleTextInput} value={text} />
       <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={() => console.log(wordsCounter(text))}>Start</button>
       <h2>Word count: ???</h2>
     </div>
   );
