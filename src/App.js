f68b00bfcb6b6a653c0fe5f570fcec889c4e43ae
Billie Amit <biliamit@gmail.com>
2022-04-12 14:16:43 +0300

    add text state and handle change function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState("");
+
+  function handleTextInput(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea onChange={handleTextInput} value={text} />
       <h4>Time remaining: ???</h4>
       <button>Start</button>
       <h2>Word count: ???</h2>
